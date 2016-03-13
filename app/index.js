766a44a211186a34482ae82517b81951dda4816d
Celia <celia@peek.com>
2016-03-12 16:13:27 -0800

    Section 3- pure functions, props, and nesting components

--- app/index.js
+++ app/index.js
@@ -1,19 +1,70 @@
+var USER_DATA = {
+  name: 'Celia',
+  username: 'cdeeter',
+  image: 'https://avatars2.githubusercontent.com/u/8375521?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// This is how you define a component
-var HelloWorld = React.createClass({
-  // Render method specifies the UI of the component using JSX
-  render: function () {
+/* Components should be:
+    Focused
+    Independent
+    Resuable
+    Small
+    Testable
+
+  (... aka FIRST)
+*/
+
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
     return (
-      <div>Hello World</div>
-    );
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
   }
 });
 
 ReactDOM.render(
-  // This is how you invoke a component
-  <HelloWorld />,
-  // Where to put the component
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
-)
+);
+
+// Pure Functions
+// Evaluates the same result given the same arguments
+// Doesn't depend on and doesn't modify the states of variables out of its scope.
+// No side effects (mutations, async reqs)
+
+// fn(d)=v === function that takes in data and outputs a view
