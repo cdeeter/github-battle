9987f5a1db23332cfada3fa89305a29aacb15500
Celia <celia@peek.com>
2016-03-20 16:48:15 -0700

    Section 10- Private functional stateless components/start over button

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -2,33 +2,34 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
 var Link = require('react-router').Link;
-var UserDetails = require('../components/UserDetails');
-var UserDetailsWrapper = require('../components/UserDetailsWrapper');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+var MainContainer = require('./MainContainer');
 
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p> Loading! </p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
-      <h1>Confirm Players</h1>
-      <div className='col-sm-8 col-sm-offset-2'>
-        <UserDetailsWrapper header='Player 1'>
-          <UserDetails info={props.playersInfo[0]} />
-        </UserDetailsWrapper>
-        <UserDetailsWrapper header='Player 2'>
-          <UserDetails info={props.playersInfo[1]} />
-        </UserDetailsWrapper>
-      </div>
-      <div className='col-sm-8 col-sm-offset-2'>
-        <div className='col-sm-12' style={styles.space}>
-          <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
+    : <MainContainer>
+        <h1>Confirm Players</h1>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <UserDetailsWrapper header='Player 1'>
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player 2'>
+            <UserDetails info={props.playersInfo[1]} />
+          </UserDetailsWrapper>
         </div>
-        <div className='col-sm-12' style={styles.space}>
-          <Link to='/playerOne'>
-            <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
-          </Link>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <div className='col-sm-12' style={styles.space}>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
+          </div>
+          <div className='col-sm-12' style={styles.space}>
+            <Link to='/playerOne'>
+              <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </div>
+      </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
